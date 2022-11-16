db3e452755531b8869b2a50a21be8f1fb84e1df3
HiPapi <96549124+HiPap@users.noreply.github.com>
2022-11-16 22:35:32 +0000

    Add files via upload

--- webhookspammer.py
+++ webhookspammer.py
@@ -1,38 +1,41 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import time
+from colorama import Fore
+import os
+
+
+os.system('cls & title Webhook X by HiPapi')
 
 
 def MainMenu():
- print('''
+ print(Fore.CYAN + '''
 
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⡟⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢹⣿⣿⣿
-⣿⣿⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⣿
-⣿⣿⣿⡇⠄⠄⠄⢠⣴⣾⣵⣶⣶⣾⣿⣦⡄⠄⠄⠄⢸⣿⣿⣿
-⣿⣿⣿⡇⠄⠄⢀⣾⣿⣿⢿⣿⣿⣿⣿⣿⣿⡄⠄⠄⢸⣿⣿⣿
-⣿⣿⣿⡇⠄⠄⢸⣿⣿⣧⣀⣼⣿⣄⣠⣿⣿⣿⠄⠄⢸⣿⣿⣿
-⣿⣿⣿⡇⠄⠄⠘⠻⢷⡯⠛⠛⠛⠛⢫⣿⠟⠛⠄⠄⢸⣿⣿⣿
-⣿⣿⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⣿
-⣿⣿⣿⣧⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢡⣀⠄⠄⢸⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣆⣸⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+ ____     ___ ______  ____   ___       __ __ 
+|    \   /  _]      ||    \ /   \     |  |  |
+|  D  ) /  [_|      ||  D  )     |    |  |  |
+|    / |    _]_|  |_||    /|  O  |    |_   _|
+|    \ |   [_  |  |  |    \|     |    |     |
+|  .  \|     | |  |  |  .  \     |    |  |  |
+|__|\_||_____| |__|  |__|\_|\___/     |__|__|
 
 
+                                                                        
 ''')
 
 
  try:
-    link = input('Enter your Webhook => ')
-    message = input('Enter your webhook content => ')
+    link = input(Fore.CYAN + 'Enter your Webhook $ ')
+    message = input(Fore.CYAN + 'Enter your webhook content $ ')
 
 
     while True:
      webhook = DiscordWebhook(url=link, content=message, rate_limit_retry=True)
-     time.sleep(4)
+     time.sleep(3)
      response = webhook.execute()
+     print(Fore.CYAN + 'Spamming webhook....')
 
  except:
-      print('Webhook has been deleted, try again!')
+      print(Fore.CYAN + 'Webhook has been deleted, try again!')
       MainMenu()
 MainMenu()
 
